4d3e8ec8d4d150b804db24f106c89b5721360a2d
Niklas de Vries <niklas@mediajungle.at>
2014-08-07 00:32:38 +0200

    added controllerName to ViewGeneratorCommand for variables in template

--- src/Way/Generators/Commands/ScaffoldGeneratorCommand.php
+++ src/Way/Generators/Commands/ScaffoldGeneratorCommand.php
@@ -7,55 +7,55 @@ use Config;
 
 class ScaffoldGeneratorCommand extends ResourceGeneratorCommand {
 
-    /**
-     * The console command name.
-     *
-     * @var string
-     */
-    protected $name = 'generate:scaffold';
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'generate:scaffold';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Scaffold a new resource (with boilerplate)';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Scaffold a new resource (with boilerplate)';
 
-    /**
-     * Call model generator if user confirms
-     *
-     * @param $resource
-     */
-    protected function callModel($resource)
-    {
-        $modelName = $this->getModelName($resource);
+	/**
+	 * Call model generator if user confirms
+	 *
+	 * @param $resource
+	 */
+	protected function callModel($resource)
+	{
+		$modelName = $this->getModelName($resource);
 
-        if ($this->confirm("Do you want me to create a $modelName model? [yes|no]"))
-        {
-            $this->call('generate:model', [
-                'modelName' => $modelName,
-                '--templatePath' => Config::get("generators::config.scaffold_model_template_path")
-            ]);
-        }
-    }
+		if ($this->confirm("Do you want me to create a $modelName model? [yes|no]"))
+		{
+			$this->call('generate:model', [
+				'modelName' => $modelName,
+				'--templatePath' => Config::get("generators::config.scaffold_model_template_path")
+			]);
+		}
+	}
 
-    /**
-     * Call controller generator if user confirms
-     *
-     * @param $resource
-     */
-    protected function callController($resource)
-    {
-        $controllerName = $this->getControllerName($resource);
+	/**
+	 * Call controller generator if user confirms
+	 *
+	 * @param $resource
+	 */
+	protected function callController($resource)
+	{
+		$controllerName = $this->getControllerName($resource);
 
-        if ($this->confirm("Do you want me to create a $controllerName controller? [yes|no]"))
-        {
-            $this->call('generate:controller', [
-                'controllerName' => $controllerName,
-                '--templatePath' => Config::get("generators::config.scaffold_controller_template_path")
-            ]);
-        }
-    }
+		if ($this->confirm("Do you want me to create a $controllerName controller? [yes|no]"))
+		{
+			$this->call('generate:controller', [
+				'controllerName' => $controllerName,
+				'--templatePath' => Config::get("generators::config.scaffold_controller_template_path")
+			]);
+		}
+	}
 
 	/**
 	 * Call view generator if user confirms
--- src/Way/Generators/Commands/ViewGeneratorCommand.php
+++ src/Way/Generators/Commands/ViewGeneratorCommand.php
@@ -6,93 +6,94 @@ use Symfony\Component\Console\Input\InputArgument;
 
 class ViewGeneratorCommand extends GeneratorCommand {
 
-    /**
-     * The console command name.
-     *
-     * @var string
-     */
-    protected $name = 'generate:view';
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'generate:view';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Generate a view';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Generate a view';
 
-    /**
-     * Create directory tree for views,
-     * and fire generator
-     */
-    public function fire()
-    {
-        $directoryPath = dirname($this->getFileGenerationPath());
+	/**
+	 * Create directory tree for views,
+	 * and fire generator
+	 */
+	public function fire()
+	{
+		$directoryPath = dirname($this->getFileGenerationPath());
 
-        if ( ! File::exists($directoryPath))
-        {
-            File::makeDirectory($directoryPath, 0777, true);
-        }
+		if ( ! File::exists($directoryPath))
+		{
+			File::makeDirectory($directoryPath, 0777, true);
+		}
 
-        parent::fire();
-    }
+		parent::fire();
+	}
 
-    /**
-     * The path where the file will be created
-     *
-     * @return mixed
-     */
-    protected function getFileGenerationPath()
-    {
-        $path = $this->getPathByOptionOrConfig('path', 'view_target_path');
-        $viewName = str_replace('.', '/', $this->argument('viewName'));
+	/**
+	 * The path where the file will be created
+	 *
+	 * @return mixed
+	 */
+	protected function getFileGenerationPath()
+	{
+		$path = $this->getPathByOptionOrConfig('path', 'view_target_path');
+		$viewName = str_replace('.', '/', $this->argument('viewName'));
 
-        return sprintf('%s/%s.blade.php', $path, $viewName);
-    }
+		return sprintf('%s/%s.blade.php', $path, $viewName);
+	}
 
-    /**
-     * Fetch the template data
-     *
-     * @return array
-     */
-    protected function getTemplateData()
-    {
-	    // LessonsController
-	    $name = ucwords($this->argument('controllerName'));
+	/**
+	 * Fetch the template data
+	 *
+	 * @return array
+	 */
+	protected function getTemplateData()
+	{
+		// LessonsController
+		$name = ucwords($this->argument('controllerName'));
 
-	    // lessons
-	    $collection = strtolower(str_replace('Controller', '', $name));
+		// lessons
+		$collection = strtolower(str_replace('Controller', '', $name));
 
-	    // lesson
-	    $resource = str_singular($collection);
+		// lesson
+		$resource = str_singular($collection);
 
-	    // Lesson
-	    $model = ucwords($resource);
+		// Lesson
+		$model = ucwords($resource);
 
-	    $path = $this->getFileGenerationPath();
+		$path = $this->getFileGenerationPath();
 
-	    return compact('name', 'collection', 'resource', 'model', 'path');
-    }
+		return compact('name', 'collection', 'resource', 'model', 'path');
+	}
 
-    /**
-     * Get path to the template for the generator
-     *
-     * @return mixed
-     */
-    protected function getTemplatePath()
-    {
-        return $this->getPathByOptionOrConfig('templatePath', 'view_template_path');
-    }
+	/**
+	 * Get path to the template for the generator
+	 *
+	 * @return mixed
+	 */
+	protected function getTemplatePath()
+	{
+		return $this->getPathByOptionOrConfig('templatePath', 'view_template_path');
+	}
 
-    /**
-     * Get the console command arguments.
-     *
-     * @return array
-     */
-    protected function getArguments()
-    {
-        return [
-            ['viewName', InputArgument::REQUIRED, 'The name of the desired view']
-        ];
-    }
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return [
+			['viewName', InputArgument::REQUIRED, 'The name of the desired view'],
+			['controllerName', InputArgument::OPTIONAL, 'The name of the desired controller']
+		];
+	}
 
 }
